81615c3284895f51fda96cb7d1b696f7de8698f5
Panji Nayaka Reswara <PanjiNares09@gmail.com>
2026-02-11 23:17:23 +0700

    feat: Membuat models dan migrations

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using _2026_LendARoom_backend.Models;
+
+namespace _2026_LendARoom_backend.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+    public DbSet<User> Users{ get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<LoanRequest> LoanRequests{ get; set;}
+}
---
+++ Models/LoanRequest.cs
@@ -0,0 +1,15 @@
+namespace _2026_LendARoom_backend.Models;
+
+public class LoanRequest
+{
+    public int Id{ get; set; }
+    public int UserId{ get; set; }
+    public int RoomId{ get; set; }
+    public DateTime RequestDate{ get; set; } = DateTime.Now;
+    public DateTime BorrowDate{ get; set; }
+    public string Description{ get; set; } = string.Empty;
+    public string Status{ get; set; } = "Pending"; //"Pending", "Approved", "Rejected", and "Completed"
+
+    public User? User{ get; set; }
+    public Room? Room{ get; set; } 
+}
